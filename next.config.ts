78c8c6994eaae5213405ff2282d6ed094e189009
MackeyK24 <MackeyK24@gmail.com>
2025-11-20 17:48:03 -1000

    8.36.1100 Next Configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // reactStrictMode: true, // Enable Strict Mode
+  // output: "export",      // Enable Static Export
 };
 
 export default nextConfig;
