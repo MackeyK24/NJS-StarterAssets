bf728c67d2d498eaf57a2b24883ae2f902aa7703
MackeyK24 <MackeyK24@gmail.com>
2026-05-07 23:08:16 -1000

    Splash Screen Updates

--- src/babylon/system/babylon.tsx
+++ src/babylon/system/babylon.tsx
@@ -14,6 +14,7 @@ import { useCallback } from "react";
 import { useUnifiedNavigation, UnifiedNavigateFunction, LocationState } from "./platform";
 import BaseSceneViewer from "./viewer";
 import CustomOverlay from "../custom/overlay";
+import SplashScreen from "../custom/splash";
 import GameManager from "../globals";
 import "./babylon.css";
 
