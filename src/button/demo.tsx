97bf3da25c31671b0e7a6423b35a3812f4eec6b1
MackeyK24 <MackeyK24@gmail.com>
2026-05-11 13:08:35 -1000

    Project Cleanup Update II

--- src/button/demo.tsx
+++ src/button/demo.tsx
@@ -1,15 +1,21 @@
 'use client';
 
 import { SceneManager, ScriptComponent, Utilities } from "@babylonjs-toolkit/next";
-import { useUnifiedNavigation } from "../babylon/system/platform";
 import GameManager from "../babylon/globals";
 
 export default function PlayDemoButton() {
-  const { navigate } = useUnifiedNavigation();
+  const handlePlayDemo = () => {
+    GameManager.NavigateTo("/play", {
+      gameMode: "DemoGameMode",
+      rootPath: GameManager.AwsPlaygroundRepo,
+      sceneFile: "samplescene.gltf",
+      importMeshes: ["playerarmature.gltf"]
+    });
+  };
 
   return (
     <div className="nx-card">
-      <button onClick={() => navigate("/play", { state: { fromApp: true, gameMode: "DemoGameMode", rootPath: GameManager.AwsPlaygroundRepo, sceneFile: "samplescene.gltf", importMeshes: ["playerarmature.gltf"], hideSplashScreen: true } })} className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]">Play Demo</button>
+      <button onClick={handlePlayDemo} className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]">Play Demo</button>
     </div>
   );
 }
