d67d6281374b692dd198ff711d4b7c707ee54d21
MackeyK24 <MackeyK24@gmail.com>
2026-05-10 14:58:27 -1000

    Sync Project Cleanup

---
+++ src/button/demo.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { SceneManager, ScriptComponent, Utilities } from "@babylonjs-toolkit/next";
+import { useUnifiedNavigation } from "../babylon/system/platform";
+import GameManager from "../babylon/globals";
+
+export default function PlayDemoButton() {
+  const { navigate } = useUnifiedNavigation();
+
+  return (
+    <div className="nx-card">
+      <button onClick={() => navigate("/play", { state: { fromApp: true, gameMode: "PlayerControllerDemo", rootPath: GameManager.AwsPlaygroundRepo, sceneFile: "samplescene.gltf", importMeshes: ["playerarmature.gltf"], hideSplashScreen: true } })} className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]">Play Demo</button>
+    </div>
+  );
+}
