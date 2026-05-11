193dad3859843570ff3ca5ce1d9316da1d4775ad
MackeyK24 <MackeyK24@gmail.com>
2026-05-10 23:40:04 -1000

    Cleanup Project Part A

--- src/button/demo.tsx
+++ src/button/demo.tsx
@@ -9,7 +9,7 @@ export default function PlayDemoButton() {
 
   return (
     <div className="nx-card">
-      <button onClick={() => navigate("/play", { state: { fromApp: true, gameMode: "PlayerControllerDemo", rootPath: GameManager.AwsPlaygroundRepo, sceneFile: "samplescene.gltf", importMeshes: ["playerarmature.gltf"], hideSplashScreen: true } })} className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]">Play Demo</button>
+      <button onClick={() => navigate("/play", { state: { fromApp: true, gameMode: "DemoGameMode", rootPath: GameManager.AwsPlaygroundRepo, sceneFile: "samplescene.gltf", importMeshes: ["playerarmature.gltf"], hideSplashScreen: true } })} className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]">Play Demo</button>
     </div>
   );
 }
